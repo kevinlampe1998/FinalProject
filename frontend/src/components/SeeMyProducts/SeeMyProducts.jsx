e7c1509f8cd5ef31a73a394abaccda12337458d4
kevinlampe1998 <kevin.lampe@dci-student.org>
2024-11-04 11:48:34 +0100

    updated SeeMyProduct.jsx again

--- frontend/src/components/SeeMyProducts/SeeMyProducts.jsx
+++ frontend/src/components/SeeMyProducts/SeeMyProducts.jsx
@@ -1,8 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import { TheContext } from "../../App";
 import './SeeMyProducts.css';
 
 const MyProduct = ({props: props}) => {
+    const productContainer = useRef();
 
     const deleteProduct = async (_id) => {
         const res = await fetch(`http://localhost:3000/used-items/${_id}`, { method: 'DELETE' });
@@ -13,14 +14,26 @@ const MyProduct = ({props: props}) => {
         location.reload();
     };
 
+    const updateProduct = async (event) => {
+        event.preventDefault();
+
+        const product_name = productContainer.current.children[0].innerHTML;
+        console.log(product_name);
+    };
+
+    const changeToForm = () => {
+        productContainer.current.children[1]        
+        
+    };
+
     return (
-        <div className="see-my-product-container">
+        <div ref={productContainer} className="see-my-product-container">
             <h4>{props.product_name}</h4>
             <img src={props.main_picture?.url} alt={props.product_name} />
             <div>Description: {props.description}</div>
             <div>Price: {props.price}</div>
             <div>
-                <button>Change</button>
+                <button onClick={changeToForm}>Change</button>
                 <button onClick={() => deleteProduct(props._id)}>Delete</button>
             </div>
         </div>
